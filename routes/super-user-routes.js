71e97e16f914a89596f785026d9886bcea847197
keval bambhaniya <kevalbambhaniya4@gmail.com>
2026-05-17 15:50:54 +0530

    Add Vercel configuration file for server deployment

---
+++ routes/super-user-routes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const { check } = require("express-validator");
+const { newUser, loginUser } = require("../controllers/userController");
+const checkAuth = require("../middleware/check-auth");
+const superUserRoutes = express.Router();
+
+superUserRoutes.post(
+  "/login",
+  [
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  loginUser
+);
+
+superUserRoutes.use(checkAuth);
+  
+superUserRoutes.post(
+  "/signup",
+  [
+    check("email").normalizeEmail().isEmail(),
+    check("name").notEmpty(),
+    check("password").isLength({ min: 8 }),
+    check("position").notEmpty(),
+    check("aadhar").notEmpty(),
+    check("panNo").notEmpty(),
+  ],
+  newUser
+);
+
+module.exports = superUserRoutes;
+
+
+// const express = require("express");
+// const { check } = require("express-validator");
+// const { newUser, loginUser } = require("../controllers/userController");
+// const checkAuth = require("../middleware/check-auth");
+
+// const superUserRoutes = express.Router();
+
+// // ✅ Public routes
+// superUserRoutes.post(
+//   "/login",
+//   [
+//     check("email").normalizeEmail().isEmail(),
+//     check("password").isLength({ min: 6 }),
+//   ],
+//   loginUser
+// );
+
+// superUserRoutes.post(
+//   "/signup",
+//   [
+//     check("email").normalizeEmail().isEmail(),
+//     check("name").notEmpty(),
+//     check("password").isLength({ min: 8 }),
+//     check("position").notEmpty(),
+//     check("aadhar").notEmpty(),
+//     check("panNo").notEmpty(),
+//   ],
+//   newUser
+// );
+
+// // 🔐 Protected routes (after this line)
+// superUserRoutes.use(checkAuth);
+
+// // (any protected APIs here)
+
+// module.exports = superUserRoutes;
